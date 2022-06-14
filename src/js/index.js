6530e94451d2b8087067a97dcad0895583dcca9e
kradchenkonlt <44407472+kriss-tinkaa@users.noreply.github.com>
2022-06-14 21:53:50 +0200

    hw-4

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 import '../styles/main.scss';
 
-let name = prompt('What is your name?', 'Leo');
-let surname = prompt('What is your surname?', 'Smith');
+// Lesson 3
 
-alert(`Ваше имя ${name} и ваша фамилия ${surname}`); 
+//let name = prompt('What is your name?', 'Leo');
+//let surname = prompt('What is your surname?', 'Smith');
+
+//alert(`Ваше имя ${name} и ваша фамилия ${surname}`);
+
+
+// Lesson 4
+
+for(let i = 0; i < 100; i++){
+    if(i % 3 == 0){
+        console.log(`кратное 3: ${i}`)
+    }
+    if(i % 5 == 0){
+        console.log(`кратное 5: ${i}`)
+    }
+    if(i % 15 == 0){
+        console.log(`кратное 15: ${i}`)
+    }
+}
